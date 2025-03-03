cdb540796227f0e404e416ce9dd3ad2132512113
Christian Jacob <ccj.99@t-online.de>
2025-03-03 15:53:17 +0100

    Add different prompting strategies
    Co-authored-by: David S. <panicmod-e@users.noreply.github.com>

--- David-Christian/utils.py
+++ David-Christian/utils.py
@@ -10,12 +10,13 @@ import requests
 import json
 import pandas as pd
 import datetime
+import os
 
 
 class MetricHelper:
 
     @staticmethod
-    def calculateRougeScore(reference, candidate) -> dict:
+    def calculate_rouge_score(reference, candidate) -> dict:
         reference = [reference]
         candidate = [candidate]
 
@@ -92,28 +93,50 @@ class InferenceHelper:
         }
 
         response = requests.post(url, headers=headers, data=json.dumps(data_json))
-        return json.loads(response.text)['response']
+        try:
+            r = json.loads(response.text)['response']
+            return r
+        except:
+            print(response.text)
+            return response.text
     
     @staticmethod
-    def generatePrompt(explanations, prompt_task, prompt_input='', prompt_output=''):
+    def generatePrompt(explanations, method, mode, df, ref_df):
+        prompt_input = PromptHelper.prompt_input_default
+        prompt_output = PromptHelper.prompt_output_default
+        if mode == 'default':
+            prompt_task = PromptHelper.prompt_task_professional
+        if mode == 'source':
+            prompt_task = PromptHelper.prompt_task_source + PromptHelper.prompt_context_source + PromptHelper.load_source_files()[method]
+        if mode == 'error':
+            test, error = PromptHelper.get_test_error(method, ref_df)
+            prompt_task = PromptHelper.prompt_task_test + PromptHelper.prompt_context_test + test + error
+        if mode == 'oneshot':
+            expl, c, d = PromptHelper.get_examples(method, df, ref_df)
+            prompt_task = PromptHelper.prompt_task_professional + PromptHelper.prompt_task_oneshot_in + expl + PromptHelper.prompt_task_oneshot_out_one + c + PromptHelper.prompt_task_oneshot_out_two + d
         input = '\n\n' + '\n\n'.join(["'''\n" + expl + "\n'''" for expl in explanations]) + '\n\n'
         prompt = prompt_task + prompt_input + input + prompt_output
         return prompt
     
     @staticmethod
-    def generateExplanationsWithMetrics(methods, df, ref_df, prompt_task, prompt_input='', prompt_output='', model='deepseek-llm:7b'):
+    def generateExplanationsWithMetrics(df, ref_df, model='deepseek-llm:7b', mode='default'):
         generated_explanations = {}
+        methods = df['File'].unique().tolist()
         for method in methods:
             method_explanations = df[(df['File'] == method)]['Explanation'].to_list()
-            prompt = InferenceHelper.generatePrompt(method_explanations, prompt_task, prompt_input, prompt_output)
-            generated_explanations[method] = json.loads(InferenceHelper.runInference(prompt, model))['explanation']
+            prompt = InferenceHelper.generatePrompt(method_explanations, method, mode, df, ref_df)
+            inference = InferenceHelper.runInference(prompt, model)
+            try:
+                generated_explanations[method] = json.loads(inference)['explanation']
+            except:
+                generated_explanations[method] = inference
 
         generated_explanations_df = pd.DataFrame.from_dict(generated_explanations, orient='index', columns=['explanation'])
         generated_explanations_df = generated_explanations_df.join(ref_df.set_index('bug')['description_c']).join(ref_df.set_index('bug')['description_d'])
 
         metric_df = InferenceHelper.calculate_metrics(generated_explanations_df)
-        metric_df.to_csv('output/' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.csv')
-        return metric_df, generated_explanations_df
+        metric_df.to_csv('output/' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") +'_' + mode + '.csv')
+        return metric_df
 
 
 
@@ -130,8 +153,52 @@ class InferenceHelper:
         df['readablity_d'] = df.apply(lambda row: MetricHelper.calculateReadability(row.description_d), axis=1)
         df['cosine_c'] = df.apply(lambda row: MetricHelper.calculateCosineSimilarity([row.explanation, row.description_c]), axis=1)
         df['cosine_d'] = df.apply(lambda row: MetricHelper.calculateCosineSimilarity([row.explanation, row.description_d]), axis=1)
-        df['rouge_c'] = df.apply(lambda row: MetricHelper.calculateRougeScore(row.description_c, row.explanation), axis=1)
-        df['rouge_d'] = df.apply(lambda row: MetricHelper.calculateRougeScore(row.description_d, row.explanation), axis=1)
+        df['rouge_c'] = df.apply(lambda row: MetricHelper.calculate_rouge_score(row.description_c, row.explanation), axis=1)
+        df['rouge_d'] = df.apply(lambda row: MetricHelper.calculate_rouge_score(row.description_d, row.explanation), axis=1)
         df['bleurt_c'] = MetricHelper.calculateBleurtScore(explanations_c, explanations)
         df['bleurt_d'] = MetricHelper.calculateBleurtScore(explanations_d, explanations)
         return df
+
+
+class PromptHelper:
+    prompt_input_default = "### Input:\n\nHere are the failure explanations:\n\n"
+
+    prompt_output_default = "### Output:\nFormat your response in valid JSON format with a single field 'explanation' of type string containing your generated explanation."
+
+    prompt_task_professional = "###Task:\nYou are a professional software developer. You are given a number of explanations describing the root cause of a software failure. Based on the given explanations, write a single explanation that contains all the information required to understand the root cause of the bug. The explanation should be succinct and without redundant information"
+
+    prompt_task_source = "###Task:\nYou are a professional software developer. You are given a number of explanations describing the root cause of a software failure. As additional context you are given the source code of the file where the error occurs. Based on the given explanations and the source code, write a single explanation that contains all the information required to understand the root cause of the bug. The explanation should be succinct and without redundant information"
+
+    prompt_context_source = "\n\n###Context:\nHere is the source code for context:\n\n"
+
+    prompt_task_test = "###Task:\nYou are a professional software developer. You are given a number of explanations describing the root cause of a software failure that occured when running a unit test. As additional context you are given the unit test and the resulting error message. Based on the given explanations, the test and error, write a single explanation that contains all the information required to understand the root cause of the test error. The explanation should be succinct and without redundant information"
+
+    prompt_context_test = "\n\n###Context:\nHere is the unit test, followed by the error message:\n\n"
+
+    prompt_task_oneshot_in = "\n\n###Example:\nHere is an example of input explanations and two possible output explanations\n\nInput:\n\n"
+
+    prompt_task_oneshot_out_one = "Output Example 1:\n\n"
+    
+    prompt_task_oneshot_out_two = "Output Example 2:\n\n"
+
+    @staticmethod
+    def load_source_files():
+        src_files = {}
+        for file in os.listdir('source_files'):
+            with open('source_files/' + file, 'r') as f:
+                method = file.replace('.java', '')
+                src_files[method] = "```java\n" + f.read() + "```\n\n"
+        return src_files
+
+    def get_test_error(method, df):
+        test = '```\n' + df[(df['bug'] == method)]['test_description'].to_string() + '```\n\n'
+        error = '```\n' + df[(df['bug'] == method)]['failure_description'].to_string() + '```\n\n'
+        return test, error
+
+    def get_examples(method, df, ref_df):
+        if method == 'HIT01_8':
+            method = 'HIT02_24'
+        expl = '\n\n' + '\n\n'.join(["'''\n" + expl + "\n'''" for expl in df[(df['File'] == method)]['Explanation'].to_list()]) + '\n\n'
+        c = '```\n' + ref_df[(ref_df['bug'] == method)]['description_c'].to_string() + '```\n\n'
+        d = '```\n' + ref_df[(ref_df['bug'] == method)]['description_d'].to_string() + '```\n\n'
+        return expl, c, d
